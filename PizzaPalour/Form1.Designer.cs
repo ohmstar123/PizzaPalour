ea9f2fd6143c1037e25750aff310502116a6d207
ohmstar123 <ohmpate114@ed.amdsb.ca>
2018-10-04 14:53:14 -0400

    needed help

--- PizzaPalour/Form1.Designer.cs
+++ PizzaPalour/Form1.Designer.cs
@@ -28,7 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.titleLabel = new System.Windows.Forms.Label();
+            this.topingsLable = new System.Windows.Forms.Label();
+            this.TopingsImput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // titleLabel
@@ -43,23 +48,70 @@
             this.titleLabel.Text = "Rams Pizza Parlour";
             this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // topingsLable
+            // 
+            this.topingsLable.AutoSize = true;
+            this.topingsLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topingsLable.ForeColor = System.Drawing.SystemColors.Control;
+            this.topingsLable.Location = new System.Drawing.Point(191, 175);
+            this.topingsLable.Name = "topingsLable";
+            this.topingsLable.Size = new System.Drawing.Size(178, 24);
+            this.topingsLable.TabIndex = 1;
+            this.topingsLable.Text = "Number of Topings:";
+            // 
+            // TopingsImput
+            // 
+            this.TopingsImput.Location = new System.Drawing.Point(388, 179);
+            this.TopingsImput.Name = "TopingsImput";
+            this.TopingsImput.Size = new System.Drawing.Size(142, 20);
+            this.TopingsImput.TabIndex = 2;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(334, 253);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(131, 53);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.outputLabel.Location = new System.Drawing.Point(255, 376);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(305, 43);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.TopingsImput);
+            this.Controls.Add(this.topingsLable);
             this.Controls.Add(this.titleLabel);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label topingsLable;
+        private System.Windows.Forms.TextBox TopingsImput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label outputLabel;
     }
 }
 
--- PizzaPalour/Form1.cs
+++ PizzaPalour/Form1.cs
@@ -12,9 +12,28 @@ namespace PizzaPalour
 {
     public partial class Form1 : Form
     {
+        int topings;
+        double total;
+
+        const double BASE = 7.00;
+        const double TOPING = 1.25;
+        //const
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //Take the toppings value from the input box and put it in the appropriate variable.
+
+
+		    // Calculate the price of the pizza and put the result in the appropriate variable.
+
+
+            //  Display the price from the variable to the screen, (see example below), formatted to 2 decimal places with dollar sign in front.
+
         }
     }
 }
